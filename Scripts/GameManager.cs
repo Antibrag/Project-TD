acf88a096f4000327437a4de1d41278ecb5db28b
San4hi <antonsayfermay@gmail.com>
2024-01-13 23:39:14 +0300

    add LoadFirstUnComplete() func

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,11 +1,28 @@
 using Godot;
-using System;
 
 public partial class GameManager : Node
 {
-    public override void _Ready()
+    public override void _Ready()   
     {
         DataController.LoadLevelsData();
         DataController.LoadPlayerData();
+
+        LoadFirstUnCompleteLevel();
+    }
+
+    private void LoadFirstUnCompleteLevel()
+    {
+        for (int i = 0; i < Data.LevelsList.Length; i++)
+        {
+            if (!Data.LevelsList[i].IsCompleted) 
+            {
+                var level = GD.Load<PackedScene>(Data.LevelsList[i].ScenePath).Instantiate();
+                GetNode<Node>("/root/Main").CallDeferred("add_child", level);
+
+                GD.Print($"Load level - {Data.LevelsList[i].Name}");
+                return;
+            }
+        }
+        GD.PushWarning("All levels complete or failed to load");
     }
 }
