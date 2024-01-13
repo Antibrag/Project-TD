2c141be2ee29d19913030a545604d0b07bf21cc6
San4hi <antonsayfermay@gmail.com>
2024-01-13 23:09:17 +0300

    rename save starting funcs

--- Scripts/DataController.cs
+++ Scripts/DataController.cs
@@ -8,7 +8,7 @@ public class DataController
     private const string PlayerDataSavePath = "user://PlayerData.json";
     private const string LevelsDataSavePath = "user://LevelsData.json";
 
-    private static void SaveStartupLevelsData()
+    private static void SaveStartingLevelsData()
     {
         using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Write);
 
@@ -21,7 +21,7 @@ public class DataController
         file.StoreLine(Json.Stringify(dict));
     }
 
-    private static void SaveStartupPlayerData()
+    private static void SaveStartingPlayerData()
     {
         using var file = FileAccess.Open(PlayerDataSavePath, FileAccess.ModeFlags.Write);
 
@@ -35,7 +35,10 @@ public class DataController
     public static void LoadLevelsData()
     {
         if (!FileAccess.FileExists(LevelsDataSavePath)) 
-            SaveStartupLevelsData();
+        {
+            GD.PushWarning("Levels data not found\nCreate starting levels information");
+            SaveStartingLevelsData();
+        }
 
         using var file = FileAccess.Open(LevelsDataSavePath, FileAccess.ModeFlags.Read);
 
@@ -47,22 +50,30 @@ public class DataController
             {
                 Data.LevelsList[i].IsCompleted = (bool) dict[Data.LevelsList[i].Name];
             }
-            catch (System.Exception ex)
+            catch (System.Exception)
             {
-                GD.PrintErr($" - Cannot load level information from file - \n{ex}");
+                GD.PrintErr($"Cannot load level data from file - {Data.LevelsList[i].Name}");
+                return;
             }
         }
+
+        GD.Print("Load data levels succsesfully");
     }
 
     public static void LoadPlayerData()
     {
         if (!FileAccess.FileExists(PlayerDataSavePath)) 
-            SaveStartupPlayerData();
+        {
+            GD.PushWarning("Player data not found\nCreate starting player information");
+            SaveStartingPlayerData();
+        }
 
         using var file = FileAccess.Open(PlayerDataSavePath, FileAccess.ModeFlags.Read);
 
         Dictionary dict = (Dictionary)Json.ParseString(file.GetAsText());
 
         //Code...
+
+        GD.Print("Load player data succsesfully");
     }
 }
