63d1446a8b32651306ac789b03c8f090b17f240a
San4hi <antonsayfermay@gmail.com>
2024-01-14 20:21:11 +0300

    add spawn mobs

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -4,6 +4,7 @@ using Godot.Collections;
 public partial class Spawner : Area3D
 {
 	private Dictionary<string, int> LevelMobs { get; set; }
+
     public override void _Ready()
     {
         LevelMobs = (Dictionary<string, int>) GetNode<Node3D>("/root/Main/Level").GetMeta("MobsCounts");
@@ -17,9 +18,19 @@ public partial class Spawner : Area3D
 
 	public void OnSpawnTimerTimeout()
 	{
-		//В будущем таймер будет брать список мобов на уровне из метаданных уровня
-		//Список копируется в локальный список спавнера
-		//Спавнер будет брать рандомного моба и инстанциировать его
-		//Если количество данного моба уже закончилось то он вычеркивается из локального списка
+		int random_mob_index = new System.Random().Next(0, Data.MobsList.Length);
+		string mob_name = Data.MobsList[random_mob_index].Name;
+
+		if (LevelMobs[mob_name] == 0)
+			LevelMobs.Remove(mob_name);
+		else
+			LevelMobs[mob_name]--;
+
+		Mob mob_instance = (Mob) GD.Load<PackedScene>(Data.MobsList[random_mob_index].ScenePath).Instantiate();
+		GetParent().CallDeferred("add_child", mob_instance);
+
+		mob_instance.Initialize(mob_name, Data.MobsList[random_mob_index].Health, Position);
+
+		GD.Print($"Create Mob - {Data.MobsList[random_mob_index].Name}");
 	}
 }
