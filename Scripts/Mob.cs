ef6f5940ae512e73cd2f0f153f3e7f9923f4cf9c
San4hi <antonsayfermay@gmail.com>
2024-01-16 21:23:23 +0300

    retype Area3D to PathFollower3D

--- Scripts/Mob.cs
+++ Scripts/Mob.cs
@@ -1,14 +1,21 @@
 using Godot;
 
-public partial class Mob : Area3D
+public partial class Mob : PathFollow3D
 {
 	public string GameName { get; set; }
 	public float Health { get; set; }
 
-    public void Initialize(string name, float health, Vector3 position)
+	[Export]
+	public float Speed { get; set; }
+
+    public void Initialize(string name, float health)
 	{
 		GameName = name;
 		Health = health;
-		Position = position;
 	}
+
+    public override void _Process(double delta)
+    {
+        ProgressRatio += Speed * (float) delta;
+    }
 }
