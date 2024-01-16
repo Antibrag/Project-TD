f2942467ec75c645f3cd64264360e5e8b4e0cb48
San4hi <antonsayfermay@gmail.com>
2024-01-16 21:24:09 +0300

    fix out of dictionary error

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -3,33 +3,41 @@ using Godot.Collections;
 
 public partial class Spawner : Area3D
 {
-	private Dictionary<string, int> LevelMobs { get; set; }
+	private Dictionary<string, int> _levelMobs { get; set; }
+	private Timer _spawnTimer;
 
     public override void _Ready()
     {
-        LevelMobs = (Dictionary<string, int>) GetNode<Node3D>("/root/Main/Level").GetMeta("MobsCounts");
+        _levelMobs = (Dictionary<string, int>) GetNode<Node3D>("/root/Main/Level").GetMeta("MobsCounts");
+		_spawnTimer = GetNode<Timer>("SpawnTimer");
     }
 
     public void StartSpawn(float spawnTime)
 	{
-		GetNode<Timer>("SpawnTimer").WaitTime = spawnTime;
-		GetNode<Timer>("SpawnTimer").Start();
+		_spawnTimer.WaitTime = spawnTime;
+		_spawnTimer.Start();
 	}
 
 	public void OnSpawnTimerTimeout()
 	{
-		int random_mob_index = new System.Random().Next(0, Data.MobsList.Length);
+		if (_levelMobs.Count == 0)
+		{
+			_spawnTimer.Stop();
+			return;
+		}
+
+		int random_mob_index = new System.Random().Next(0, _levelMobs.Count);
 		string mob_name = Data.MobsList[random_mob_index].Name;
 
-		if (LevelMobs[mob_name] == 0)
-			LevelMobs.Remove(mob_name);
+		if (_levelMobs[mob_name] == 0)
+			_levelMobs.Remove(mob_name);
 		else
-			LevelMobs[mob_name]--;
+			_levelMobs[mob_name]--;
 
 		Mob mob_instance = (Mob) GD.Load<PackedScene>(Data.MobsList[random_mob_index].ScenePath).Instantiate();
-		GetParent().CallDeferred("add_child", mob_instance);
+		GetNode<Path3D>(GetParent().GetPath() + "/Path3D").CallDeferred("add_child", mob_instance);
 
-		mob_instance.Initialize(mob_name, Data.MobsList[random_mob_index].Health, Position);
+		mob_instance.Initialize(mob_name, Data.MobsList[random_mob_index].Health);
 
 		GD.Print($"Create Mob - {Data.MobsList[random_mob_index].Name}");
 	}
