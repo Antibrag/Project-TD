67ce6fa70896791bd13010284fe89abcd9c32fe8
San4hi <antonsayfermay@gmail.com>
2024-04-02 19:04:02 +0300

    fix targets system

--- Scripts/Player/Bulds/CrossBow.cs
+++ Scripts/Player/Bulds/CrossBow.cs
@@ -7,9 +7,12 @@ namespace Builds
 	public partial class CrossBow : Area3D
 	{
 		[Export] public Timer AttackCDTimer;
+		[Export] public MeshInstance3D TargetIndicator;
+
 		public Storage.Build Characteristics { get; set; }
 
-		private Level.Mob Target { get; set; }
+		private System.Collections.Generic.List<Level.Mob> TargetsList { get; set; } = new();
+		private Level.Mob Target { get; set; } = null;
 
 		private bool IsPossibilityPlace { get; set; }
 		private bool IsPlaced { get; set; }
@@ -18,10 +21,23 @@ namespace Builds
 		{
 			for (int i = 0; i < Storage.BuildsList.Length; i++)
 				if (Storage.BuildsList[i].Name == "CrossBow")
-					Characteristics = Storage.BuildsList[i];
+					Characteristics = (Storage.Build) Storage.BuildsList[i].Clone();
 			
 			IsPlaced = false;
 			GetNode<CollisionShape3D>("AttackRadius").Disabled = true;
+		}
+
+		private void NextTarget()
+		{
+			if (TargetsList.Count == 1)
+			{
+				Target = null;
+				TargetIndicator.GlobalPosition = Vector3.Zero;
+				return;
+			}
+
+			TargetsList.Remove(Target);
+			Target = TargetsList[0];
 		}
 
 		private Vector3 ScreenPointToRay()
@@ -44,41 +60,73 @@ namespace Builds
 			return (Vector3) rayArray["position"];
 		}
 
-		public void OnAreaEntered(Area3D enteredArea)
+		private void MoveToMouse()
+		{
+			Vector3 MousePosition = ScreenPointToRay();
+
+			Position = new Vector3
+			(
+				MousePosition != Vector3.Zero ? MousePosition.X : Position.X,
+				Position.Y,
+				MousePosition != Vector3.Zero ? MousePosition.Z : Position.Z
+			);
+		}
+
+		public void OnAreaEntered(Area3D enteredArea) 
+		{
+			if (!IsPlaced)
+				IsPossibilityPlace = false;
+			else
+			{
+				if (enteredArea.IsInGroup("Mob"))
+				{
+					Level.Mob mob = (Level.Mob) enteredArea.GetParent(); 
+
+					if (!TargetsList.Contains(mob))
+						TargetsList.Add(mob);	
+
+					if (TargetsList.Count == 1)
+						Target = mob;	
+				}
+			}
+		}
+			
+		public void OnAreaExited(Area3D exitedArea) 
 		{
 			if (!IsPlaced)
 			{
-				IsPossibilityPlace = false;
+				IsPossibilityPlace = true;
+				return;
 			}
-			/*else
-			{
 
-			}*/
+			NextTarget();
 		}
+
 
 		public override void _Ready()
-		{
-			Initialize();
-		}
+			=> Initialize();
 
 		public override void _Input(InputEvent @ev)
 		{
 			if (ev.IsActionPressed("PastBuild") && !IsPlaced && IsPossibilityPlace)
+			{
 				IsPlaced = true;
+				GetNode<CollisionShape3D>("AttackRadius").Disabled = false;
+				GD.Print(Characteristics.Name + " - has been placed");
+			}
 		}
 
 		public override void _Process(double delta)
 		{
 			if (!IsPlaced)
-			{
-				Vector3 MousePosition = ScreenPointToRay();
+				MoveToMouse();
+				
+			if (IsPlaced && Target != null && Target.Characteristics.Health <= 0)
+				NextTarget();
+			else if (IsPlaced && Target != null)
+				TargetIndicator.GlobalPosition = new Vector3(Target.GlobalPosition.X, Target.GlobalPosition.Y+1, Target.GlobalPosition.Z);
 
-				Position = new Vector3(
-					MousePosition != Vector3.Zero ? MousePosition.X : Position.X,
-					Position.Y,
-					MousePosition != Vector3.Zero ? MousePosition.Z : Position.Z
-				);
-			}
+			GD.Print($"Target - {Target}\nTargetsListSize - {TargetsList.Count}");
 		}
 	}
 }
