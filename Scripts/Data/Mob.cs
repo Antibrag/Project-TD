0113a8f9029b459a378b2718d324240eda139987
San4hi <antonsayfermay@gmail.com>
2024-04-21 23:51:10 +0300

    add protectionPercentage to mob

--- Scripts/Data/Mob.cs
+++ Scripts/Data/Mob.cs
@@ -4,16 +4,18 @@ namespace Data
     {
         public float Health { get; set; }
         public float AttackPower { get; private set; }
+        public float ProtectionPercentage { get; private set; }
 
-        public Mob(in MeshObject mesh, float health, float attackPower)
+        public Mob(in MeshObject mesh, float health, float attackPower, float protectionPercentage)
         {
-             Mesh = mesh;
+            Mesh = mesh;
             Health = health;
             AttackPower = attackPower;
+            ProtectionPercentage = protectionPercentage;
         }
 
         public object Clone() 
-            => new Mob(Mesh, Health, AttackPower);
+            => new Mob(Mesh, Health, AttackPower, ProtectionPercentage);
             
     }    
 }
