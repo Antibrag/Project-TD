c0d09c0e551ad245f0cddde3ad8868ae869986a4
San4hi <antonsayfermay@gmail.com>
2024-04-21 23:51:23 +0300

    add flightSpeed to projectile

--- Scripts/Data/Projectile.cs
+++ Scripts/Data/Projectile.cs
@@ -4,16 +4,18 @@ namespace Data
     {
         public float AttackPower { get; set; }
         public float PenetrationPower { get; set; } //Пробивная способность
+        public float FlightSpeed { get; set;}
 
-        public Projectile(in MeshObject mesh, int level, float attackPower, float penetrationPower)
+        public Projectile(in MeshObject mesh, int level, float attackPower, float penetrationPower, float flightSpeed)
         {
             Mesh = mesh;
             Level = level;
             AttackPower = attackPower;
             PenetrationPower = penetrationPower;
+            FlightSpeed = flightSpeed;
         }
 
         public object Clone() 
-            => new Projectile(Mesh, Level, AttackPower, PenetrationPower);
+            => new Projectile(Mesh, Level, AttackPower, PenetrationPower, FlightSpeed);
     }
 }
