669d473da3f72be70bffd7c3549e9452d185cd66
San4hi <antonsayfermay@gmail.com>
2024-04-30 21:00:43 +0300

    update mana$health decrease funcs

--- Scripts/UI/HUD.cs
+++ Scripts/UI/HUD.cs
@@ -3,21 +3,51 @@ using System;
 
 public partial class HUD : Control
 {
-	private TextureProgressBar ManaBar;
-	private TextureProgressBar HealthBar;
+	private TextureProgressBar _manaBar;
+	private TextureProgressBar _healthBar;
+	private Timer _decreasedTimer;
+
+	private float _decreasedMana = 0;
+	private float _decreasedHealth = 0;
 
     public override void _Ready()
     {
-        ManaBar = GetNode<TextureProgressBar>("ManaBar");
-		HealthBar = GetNode<TextureProgressBar>("HealthBar");
+        _manaBar = GetNode<TextureProgressBar>("ManaBar");
+		_healthBar = GetNode<TextureProgressBar>("HealthBar");
+		_decreasedTimer = GetNode<Timer>("DecreasedTimer");
 
-		HealthBar.Value = HealthBar.MaxValue;
-		ManaBar.Value = HealthBar.MaxValue;	
+		_healthBar.Value = _healthBar.MaxValue;
+		_manaBar.Value = _healthBar.MaxValue;	
     }
 
-    public void ChangeManaBarValue(float newValue)
-		=> ManaBar.Value = newValue;
+    public void AddDecreasingHealth(float value)
+	{
+		_decreasedHealth += value;
+		_decreasedTimer.Start();
+	}
 
-	public void ChangeHealthBarValue(float newValue)
-		=> HealthBar.Value = newValue;
+	public void AddDecreasingMana(float value)
+	{
+		_decreasedMana += value;
+		_decreasedTimer.Start();
+	}
+
+	public void OnDecreasedTimerTimeout()
+	{
+		if (_decreasedHealth == 0 && _decreasedMana == 0)
+		{
+			_decreasedTimer.Stop();
+			return;
+		}
+
+		float decreaseHealth = _decreasedHealth * 3 / 100;
+		float decreaseMana = _decreasedMana * 3 / 100;
+
+		_healthBar.Value -= decreaseHealth;
+		_decreasedHealth -= decreaseHealth;
+
+		_manaBar.Value -= decreaseMana;
+		_decreasedMana -= decreaseMana;
+		
+	}
 }
