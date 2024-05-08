3dbf45dd96eebcba2c81e4b1afa782e6d818813d
San4hi <antonsayfermay@gmail.com>
2024-05-08 17:37:29 +0300

    replace hud class to ui namespace

--- Scripts/UI/HUD.cs
+++ Scripts/UI/HUD.cs
@@ -1,63 +1,66 @@
 using Godot;
 
-public partial class HUD : Control
+namespace UI
 {
-	private TextureProgressBar _manaBar;
-	private TextureProgressBar _healthBar;
-	private Timer _decreasedTimer;
-
-	private float _decreasedMana = 0;
-	private float _decreasedHealth = 0;
-
-	private void InitializeButtons()
+	public partial class HUD : Control
 	{
-		HBoxContainer buildbar = GetNode<HBoxContainer>("Buildbar");
+		private TextureProgressBar _manaBar;
+		private TextureProgressBar _healthBar;
+		private Timer _decreasedTimer;
 
-		for (int i = 0; i < buildbar.GetChildCount(); i++)
+		private float _decreasedMana = 0;
+		private float _decreasedHealth = 0;
+
+		private void InitializeButtons()
 		{
-			BuildSkill_Button button = (BuildSkill_Button) buildbar.GetChild(i);
-			button.Initialize(i, nameof(Build));
-		}
-	}
+			HBoxContainer buildbar = GetNode<HBoxContainer>("Buildbar");
 
-    public override void _Ready()
-    {
-        _manaBar = GetNode<TextureProgressBar>("ManaBar");
-		_healthBar = GetNode<TextureProgressBar>("HealthBar");
-		_decreasedTimer = GetNode<Timer>("DecreasedTimer");
-
-		_healthBar.Value = _healthBar.MaxValue;
-		_manaBar.Value = _healthBar.MaxValue;	
-    }
-
-    public void AddDecreasingHealth(float value)
-	{
-		_decreasedHealth += value;
-		_decreasedTimer.Start();
-	}
-
-	public void AddDecreasingMana(float value)
-	{
-		_decreasedMana += value;
-		_decreasedTimer.Start();
-	}
-
-	public void OnDecreasedTimerTimeout()
-	{
-		if (_decreasedHealth == 0 && _decreasedMana == 0)
-		{
-			_decreasedTimer.Stop();
-			return;
+			for (int i = 0; i < buildbar.GetChildCount(); i++)
+			{
+				BuildSkill_Button button = (BuildSkill_Button) buildbar.GetChild(i);
+				button.Initialize(i, nameof(Build));
+			}
 		}
 
-		float decreaseHealth = _decreasedHealth * 3 / 100;
-		float decreaseMana = _decreasedMana * 3 / 100;
+		public override void _Ready()
+		{
+			_manaBar = GetNode<TextureProgressBar>("ManaBar");
+			_healthBar = GetNode<TextureProgressBar>("HealthBar");
+			_decreasedTimer = GetNode<Timer>("DecreasedTimer");
 
-		_healthBar.Value -= decreaseHealth;
-		_decreasedHealth -= decreaseHealth;
+			_healthBar.Value = _healthBar.MaxValue;
+			_manaBar.Value = _healthBar.MaxValue;	
+		}
 
-		_manaBar.Value -= decreaseMana;
-		_decreasedMana -= decreaseMana;
-		
+		public void AddDecreasingHealth(float value)
+		{
+			_decreasedHealth += value;
+			_decreasedTimer.Start();
+		}
+
+		public void AddDecreasingMana(float value)
+		{
+			_decreasedMana += value;
+			_decreasedTimer.Start();
+		}
+
+		public void OnDecreasedTimerTimeout()
+		{
+			if (_decreasedHealth == 0 && _decreasedMana == 0)
+			{
+				_decreasedTimer.Stop();
+				return;
+			}
+
+			float decreaseHealth = _decreasedHealth * 3 / 100;
+			float decreaseMana = _decreasedMana * 3 / 100;
+
+			_healthBar.Value -= decreaseHealth;
+			_decreasedHealth -= decreaseHealth;
+
+			_manaBar.Value -= decreaseMana;
+			_decreasedMana -= decreaseMana;
+			
+		}
 	}
 }
