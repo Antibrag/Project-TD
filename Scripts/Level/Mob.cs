b52095a13348e2f65d455a4ea849ff3d82457a74
San4hi <antonsayfermay@gmail.com>
2024-05-16 19:34:46 +0300

    del unuse using in mob script

--- Scripts/Level/Mob.cs
+++ Scripts/Level/Mob.cs
@@ -1,66 +1,66 @@
-using System.Reflection.PortableExecutable;
 using Godot;
 
 namespace LevelObjects
 {
-	public partial class Mob : PathFollow3D
-	{
-		[Export] public float Speed { get; private set; }
-		[Export] public ProgressBar HealthBar { get; private set; }
+    public partial class Mob : PathFollow3D
+    {
+        [Export] public float Speed { get; private set; }
+        [Export] public ProgressBar HealthBar { get; private set; }
 
-		public Data.Mob Characteristics { get; set; }
-		public Build AttackingBuild { get; set; } = null;
+        public Data.Mob Characteristics { get; set; }
+        public Build AttackingBuild { get; set; } = null;
 
-		public void Initialize(string name) 
-		{
-			Characteristics = (Data.Mob) Data.Storage.MobsList[name].Clone();
+        public void Initialize(string name)
+        {
+            Characteristics = (Data.Mob)Data.Storage.MobsList[name].Clone();
 
-			MeshInstance3D mob_mesh = GetNode<MeshInstance3D>("Area3D/Mesh");
+            MeshInstance3D mob_mesh = GetNode<MeshInstance3D>("Area3D/Mesh");
 
-			mob_mesh.Mesh = GD.Load<Mesh>($"res://Assets/Meshes/Mobs/{name}.res");
+            mob_mesh.Mesh = GD.Load<Mesh>($"res://Assets/Meshes/Mobs/{name}.res");
 
-			HealthBar.MaxValue = Characteristics.Health;
-			HealthBar.Value = Characteristics.Health;
-		}
+            HealthBar.MaxValue = Characteristics.Health;
+            HealthBar.Value = Characteristics.Health;
+        }
 
-		public void OnBodyEntered(Node3D node)
-		{
-			if (node.Name == nameof(Player))
-			{	
-				GetNode<Player>(node.GetPath()).TakeDamage(Characteristics.AttackPower);
-				Death();
-			}
-			else if (node.Name == nameof(Data.Projectile))
-			{
-				Projectile projectile = GetNode<Projectile>(node.GetPath());
-				TakeDamage(projectile.Characteristics.AttackPower, projectile.Characteristics.PenetrationPower);
-				projectile.QueueFree();
-			}
-		}
+        public void OnBodyEntered(Node3D node)
+        {
+            if (node.Name == nameof(Player))
+            {
+                GetNode<Player>(node.GetPath()).TakeDamage(Characteristics.AttackPower);
+                Death();
+            }
+            else if (node.Name == nameof(Data.Projectile))
+            {
+                Projectile projectile = GetNode<Projectile>(node.GetPath());
+                TakeDamage(projectile.Characteristics.AttackPower, projectile.Characteristics.PenetrationPower);
+                projectile.QueueFree();
+            }
+        }
 
-		public void TakeDamage(float damageValue, float penetrationPower)
-		{
-			Characteristics.Health -= damageValue * (100 - Characteristics.ProtectionPercentage + penetrationPower) / 100;
-			HealthBar.Value = Characteristics.Health;
+        public void TakeDamage(float damageValue, float penetrationPower)
+        {
+            Characteristics.Health -= damageValue * (100 - Characteristics.ProtectionPercentage + penetrationPower) / 100;
+            HealthBar.Value = Characteristics.Health;
 
-			if (Characteristics.Health <= 0)
-				Death();
-		}
+            if (Characteristics.Health <= 0)
+                Death();
+        }
 
-		public void Death()
-		{
-			Characteristics.Health = 0;
+        public void Death()
+        {
+            Characteristics.Health = 0;
 
-			if (AttackingBuild != null)
-				AttackingBuild.NextTarget();
+            if (AttackingBuild != null)
+                AttackingBuild.NextTarget();
 
-			QueueFree();
-		}
+            QueueFree();
+        }
 
         public override void _PhysicsProcess(double delta)
-		{
-			Progress += (float) delta * Speed;
-		}
-	}	
+        {
+            Progress += (float)delta * Speed;
+        }
+    }
 }
+
 
