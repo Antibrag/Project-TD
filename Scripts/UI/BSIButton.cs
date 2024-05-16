bf7a9256236d7d68c044fa8cd2f9a24729fd752d
San4hi <antonsayfermay@gmail.com>
2024-05-16 19:30:34 +0300

    fix initialize keys for BSIbuttons

--- Scripts/UI/BSIButton.cs
+++ Scripts/UI/BSIButton.cs
@@ -3,47 +3,49 @@ using Godot;
 
 namespace UI
 {
-	public partial class BSIButton : TextureButton
-	{
-		public string ButtonName { get; set; }
-		public string BSIName { get; set; } //Build-Spell_Item name 
+    public partial class BSIButton : TextureButton
+    {
+        public string ButtonName { get; set; }
+        public string BSIName { get; set; } //Build-Spell_Item name 
 
-		public void Initialize(int actionIndex, string buttonType)
-		{
-			Disabled = false;
+        public void Initialize(int actionIndex, string buttonType)
+        {
+            Disabled = false;
 
-			switch (buttonType)
-			{
-				case nameof(Build):
-					ButtonName = $"{actionIndex+1}BButton";
-				break;
+            switch (buttonType)
+            {
+                case nameof(Build):
+                    ButtonName = $"{actionIndex + 1}BButton";
+                    break;
 
-				//case nameof(Spell):
-				//	ButtonName = $"{actionIndex}SButton";
-				//break;
+                    //case nameof(Spell):
+                    //	ButtonName = $"{actionIndex}SButton";
+                    //break;
 
-				//case nameof(Item):
-				//	ButtonName = $"{actionIndex}IButton";
-				//break;
-			}
+                    //case nameof(Item):
+                    //	ButtonName = $"{actionIndex}IButton";
+                    //break;
+            }
 
-			Data.BSIButton buttonConfiguration = Storage.BuildButtonsList[ButtonName];
+            Data.BSIButton buttonConfiguration = Storage.BuildButtonsList[ButtonName];
 
-			InputEventKey inputEventKey = new() { Keycode = buttonConfiguration.ShortcutKey };
-			Shortcut.Events.Add(inputEventKey);
+            Shortcut = new();
+            Shortcut.Events = (Godot.Collections.Array)new Godot.Collections.Array();
+            Shortcut.Events.Add(new InputEventKey() { Keycode = buttonConfiguration.ShortcutKey });
 
-			BSIName = buttonConfiguration.BSIName;
-			GetNode<Label>("KeyBind").Text = buttonConfiguration.ShortcutKey.ToString();
+            BSIName = buttonConfiguration.BSIName;
+            GetNode<Label>("KeyBind").Text = buttonConfiguration.ShortcutKey.ToString();
 
-			TextureNormal = GD.Load<CompressedTexture2D>(buttonConfiguration.ButtonTexturePath);
+            TextureNormal = GD.Load<CompressedTexture2D>(buttonConfiguration.ButtonTexturePath);
 
-		}
+            GD.Print($"Initialize button({ButtonName}, shortcut - {buttonConfiguration.ShortcutKey})");
+        }
 
-		public void OnPressed()
-		{
-			GD.Print($"Button({Name}) Pressed, BSIName = {BSIName}");
-			GetNode<Player>("/root/Main/Level/Player").PastBuild(BSIName);
-		}
-	}	
+        public void OnPressed()
+        {
+            GD.Print($"Button({Name}) Pressed, ShortCut key = {Shortcut.Events[0].ToString()}, eviri");
+            GetNode<Player>("/root/Main/Level/Player").PastBuild(BSIName);
+        }
+    }
 }
 
