266f28b1199a59f00970fb91f56502518f8f6c82
San4hi <antonsayfermay@gmail.com>
2024-05-24 21:05:07 +0300

    add BSI buttons configuration path

--- Scripts/Data/Storage.cs
+++ Scripts/Data/Storage.cs
@@ -7,6 +7,7 @@ namespace Data
     {
         public static readonly string PlayerDataSavePath = "user://PlayerData.json";
         public static readonly string LevelsDataSavePath = "user://LevelsData.json";
+        public static readonly string BSIButtonsConfigurationPath = "user://BSIButtonsConfiguration.json";
         public static string CurrentLevel;
 
         private static readonly Dictionary<string, Projectile> ProjectilesList = new Dictionary<string, Projectile>()
@@ -32,6 +33,7 @@ namespace Data
             {"CrossBow", new Build(
                 new BuildMesh(new Vector3(0,0,0), new Vector3(0.2f,0.4f,0)),
                 1, //Level
+                25, //ManaCost
                 1, //AttackSpeed
                 3,
                 new Dictionary<string, Projectile>() { {"Wood Arrow", ProjectilesList["Wood Arrow"]} }
